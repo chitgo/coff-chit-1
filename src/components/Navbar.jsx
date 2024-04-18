f0746418a5b4487a05bd14e8e4ba27c756a7e507
chitgo <kostas.chitos@gmail.com>
2024-04-19 02:47:26 +0300

    change

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,21 +3,29 @@ import searchIcon from '../assets/search-icon.svg'
 import { AiOutlineMenu } from 'react-icons/ai'
 function Navbar() {
   return (
-    <nav className="container mx-auto flex justify-between items-center py-2">
-      <img src={logo} alt="" />
-      <div>
+    <nav className="container flex items-center justify-between py-2 mx-auto">
+      <img src={logo} alt="logo" className="cursor-pointer" />
+      <div className="">
         <ul className="sm:hidden">
           <AiOutlineMenu className="text-[18px]" />
         </ul>
-        <ul className="hidden sm:flex gap-4">
-          <li className="capitalize text-[#EE8542] text-[18px] font-medium">
+        <ul className="hidden gap-4 sm:flex">
+          <li className="capitalize text-[#EE8542] text-[18px] font-medium cursor-pointer ">
             Home
           </li>
-          <li className="capitalize  text-[18px] font-medium">About</li>
-          <li className="capitalize  text-[18px] font-medium">Services</li>
-          <li className="capitalize  text-[18px] font-medium">Blog</li>
-          <li className="capitalize  text-[18px] font-medium">Contact</li>
-          <li className="capitalize  text-[18px] font-medium"></li>
+          <li className="hover:text-[#EE8542] cursor-pointer capitalize  text-[18px] font-medium">
+            About
+          </li>
+          <li className="capitalize  text-[18px] font-medium hover:text-[#EE8542] cursor-pointer">
+            Services
+          </li>
+          <li className="capitalize  text-[18px] font-medium hover:text-[#EE8542] cursor-pointer">
+            Blog
+          </li>
+          <li className="capitalize  text-[18px] font-medium hover:text-[#EE8542] cursor-pointer">
+            Contact
+          </li>
+          <li className="capitalize  text-[18px] font-medium hover:text-[#EE8542] cursor-pointer"></li>
           <img src={searchIcon} alt="" />
         </ul>
       </div>
